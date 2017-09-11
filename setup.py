f3e2e60b119dec85c738207741ce22a8e5583229
Kat Busch <kat@sidewalklabs.com>
2017-09-11 15:50:18 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="doppelganger",
-    version="0.1.0",
+    version="0.1.1",
     description='Population synthesis library',
     author='Kat Busch, Kael Greco and contributors',
     author_email='doppelganger@sidewalklabs.com',
