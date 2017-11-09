8251b90b3da42f8070277ea6764bc5f136e9bd35
Brett Naul <brettnaul@gmail.com>
2017-11-09 15:47:28 -0800

    Install pomegranate from wheel and update CircleCI setup (#50)
    * Install pomegranate from wheel and update CircleCI setup
    
    * Bump package version to 0.1.3

--- setup.py
+++ setup.py
@@ -4,31 +4,29 @@
 
 from setuptools import setup
 
+
+def get_requirements():
+    """Return the components defined in requirements.txt."""
+    with open('requirements.txt') as fin:
+        return [l.split()[0].split('#', 1)[0].strip() for l in fin
+                if l and not l.startswith('git')]
+
+
 setup(
     name="doppelganger",
-    version="0.1.2",
+    version="0.1.3",
     description='Population synthesis library',
     author='Kat Busch, Kael Greco and contributors',
     author_email='doppelganger@sidewalklabs.com',
     packages=['doppelganger'],
-    install_requires=[
-        'cvxpy==0.4.8',
-        'numpy>=1.11.0',
-        'pandas>=0.19.0',
-        'pomegranate==0.7.1',
-        'requests>=2.0.0',
-        'six>=1.10.0',
-        'future>=0.16.0',
-        'psycopg2==2.7.3.1',
-        'enum34',
-    ],
+    install_requires=get_requirements(),
     extras_require={
         'tests': [
             'flake8>=3.5.0',
             'mock>=2.0.0',
-            'nose>=1.3.4',
             'coveralls>=1.1',
             'pytest',
+            'pytest-cov',
         ],
     },
 
