91b980aa9a46a08c31c5fa91c8336d98a0e6a833
Brett Naul <brett@sidewalklabs.com>
2017-11-09 16:06:17 -0800

    Bump version to add missing requirements file

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_requirements():
 
 setup(
     name="doppelganger",
-    version="0.1.3",
+    version="0.1.4",
     description='Population synthesis library',
     author='Kat Busch, Kael Greco and contributors',
     author_email='doppelganger@sidewalklabs.com',
